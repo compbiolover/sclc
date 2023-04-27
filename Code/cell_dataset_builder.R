d8d72943b997887a06ae6d1dae57d265bf81e439
Andrew Willems <awillemscf@gmail.com>
2023-04-27 16:27:38 -0400

    Updated miRNA function

--- Code/cell_dataset_builder.R
+++ Code/cell_dataset_builder.R
@@ -1,93 +1,199 @@
-#Name: cell_dataset_builder.R
-#Author: Andrew Willems <awillems@vols.utk.edu>
-#Purpose: Efficiently build cell data set objects
-#and get pseudotime information.
+# Name: cell_dataset_builder.R
+# Author: Andrew Willems <awillems@vols.utk.edu>
+# Purpose: Efficiently build cell data set objects and get pseudotime information.
+# Documentation:
 
-cell_dataset_builder <- function(vim_genes = vim,
-                                 cell_data = denoised_sc,
-                                 cell_meta = gene_metadata,
-                                 norm_flag = c("log"),
-                                 graph_root = 1.5,
-                                 show_traj = TRUE,
-                                 point_size = 2.5,
-                                 my_root   = "Y_19",
-                                 my_monocle_graph = "pseudotime_graph.svg",
-                                 my_monocle_graph_genes = "genes_graph.svg",
-                                 my_moncole_plot_type = "svg",
-                                 my_monocle_plot_dpi = 300,
-                                 my_monocle_width = 7.5,
-                                 my_monocle_height = 7.5,
-                                 my_monocle_unit = "in",
-                                 my_cds_filename = "cds_output.rds",
-                                 my_pt_data_filename = "pt_data.csv") {
-  #Required packages
-  require(ggplot2)
-  require(monocle3)
+# Required packages
+# ggplot2: for plotting
+# igraph: for building graphs
+# monocle3: for building cell data set objects and ordering cells by pseudotime
+
+# Function arguments
+# The cell_dataset_builder function takes several arguments:
+#   
+# gene_expression: a character vector specifying the gene expression to plot.
+# gene_title: a character string specifying the gene expression to use in plot titles.
+# cell_data: a matrix of gene expression data, with rows as cells and columns as genes.
+# gene_meta: a dataframe with gene metadata.
+# cell_meta: a dataframe with cell metadata.
+# norm_flag: a character string specifying the normalization method to use.
+# umap_seed: an integer specifying the seed for the UMAP algorithm.
+# cluster_res: a numeric value specifying the resolution for clustering cells.
+# n_dims: an integer specifying the number of dimensions to use for dimension reduction (PCA only).
+# reduction_method: a character string specifying the dimension reduction method to use.
+# reduce_prep_method: a character string specifying the dimension reduction method for the preprocessing method.
+# preprocess_method: a character string specifying the preprocessing method to use after dimension reduction.
+# graph_root: a numeric value specifying the position of the root node in the pseudotime ordering graph.
+# show_traj: a logical value indicating whether to show the trajectory graph.
+# point_size: a numeric value specifying the size of the data points in the plots.
+# pt_root: a character string specifying the name of the root node in the pseudotime ordering graph.
+# pt_title: a character string specifying the pseudotime label to use in plot titles.
+# plot_file: a character string specifying the filename to save the expression graph plot to.
+# plot_type: a character string specifying the type of file to save the plot as.
+# use_ordering_funct: a logical value indicating whether to use a function to order cells by pseudotime.
+# plot_dpi: an integer specifying the resolution to save the plot as.
+# plot_width: a numeric value specifying the width of the plot.
+# plot_height: a numeric value specifying the height of the plot.
+# plot_unit: a character string specifying the unit of measurement for the plot dimensions.
+# cds_filename: a character string specifying the filename to save the CDS object as.
+# pt_file: a character string specifying the filename to save the pseudotime plot to.
+# pt_data_filename: a character string specifying the filename to save the pseudotime data to.
+ 
+# Function output
+# The cell_dataset_builder function returns a list with the following elements:
+#   
+# CDS: the cell data set object.
+# Cell_Progression_Graph: the expression graph plot.
+# PT_Graph: the pseudotime plot.
+
+
+# Required packages
+library(ggplot2)
+library(igraph)
+library(monocle3)
+
+cell_dataset_builder <- function(
+    gene_expression = vim,
+    gene_title = "VIM",
+    cell_data = denoised_sc,
+    gene_meta = gene_metadata,
+    cell_meta = cell_metadata,
+    norm_flag = "none",
+    umap_seed = 123,
+    cluster_res = 1e-5,
+    n_dims = 100,
+    reduction_method = "UMAP",
+    reduce_prep_method = "PCA",
+    preprocess_method = "PCA",
+    graph_root = 1.5,
+    show_traj = TRUE,
+    point_size = 2.5,
+    pt_root = "Y_19",
+    pt_title = "TUBA1A",
+    plot_file = "expression_graph.png",
+    plot_type = "png",
+    use_ordering_funct = TRUE,
+    plot_dpi = 600,
+    plot_width = 7,
+    plot_height = 7,
+    plot_unit = "in",
+    cds_filename = "cds.rds",
+    pt_file = "pt_graph.png",
+    pt_data_filename = "pt_data.csv") {
   
-  #Return list
-  cds_return_list <- list()
+  # Make the CDS object
+  cds <- new_cell_data_set(
+    expression_data = cell_data,
+    gene_metadata = gene_meta,
+    cell_metadata = cell_meta
+  )
+
+  cds <- preprocess_cds(cds,
+    num_dim = n_dims, method = preprocess_method,
+    norm_method = norm_flag
+  )
   
-  #Making the CDS object
-  vim_genes <- vim_genes
-  cell_data_set <- new_cell_data_set(cell_data, gene_metadata = cell_meta)
-  cds <- preprocess_cds(cell_data_set,
-   num_dim = 100,
-    method = "PCA",
-    norm_method = norm_flag)
-    
-  cds <- reduce_dimension(cds, reduction_method = "UMAP")
-  cds <- cluster_cells(cds, random_seed = 123)
-  partition_plot <- plot_cells(cds, color_cells_by = "partition")
-  vim_plot <- plot_cells(cds, genes = vim_genes, cell_size = point_size)
+  cds <- reduce_dimension(cds,
+    reduction_method = reduction_method,
+    preprocess_method = reduce_prep_method
+  )
+  plot_cells(cds, genes = gene_expression)
+  cds <- cluster_cells(cds, random_seed = umap_seed, resolution = cluster_res)
   cds <- learn_graph(cds)
-  cds <- order_cells(cds, root_pr_nodes = my_root)
-  pt_graph <- plot_cells(cds         = cds,
-             color_cells_by         = "pseudotime",
-             label_cell_groups      = FALSE,
-             label_leaves           = FALSE,
-             label_branch_points    = TRUE,
-             label_principal_points = TRUE,
-             graph_label_size       = graph_root,
-             show_trajectory_graph  = show_traj,
-             cell_size              = point_size)
+
+
+  # Function to calculate the lowest expression point for the gene and select
+  # it as the root of pseudotime ordering
+  get_earliest_principal_node <- function(cds) {
+    cell_ids <- which.min(colData(cds_test)[, "Size_Factor"])
+
+    closest_vertex <-
+      cds@principal_graph_aux[["UMAP"]]$pr_graph_cell_proj_closest_vertex
+    closest_vertex <- as.matrix(closest_vertex[colnames(cds), ])
+    root_pr_nodes <-
+      igraph::V(principal_graph(cds)[["UMAP"]])$name[as.numeric(names
+      (which.max(table(closest_vertex[cell_ids, ]))))]
+
+    root_pr_nodes
+  }
+
+  if (use_ordering_funct == TRUE) {
+    cds <- order_cells(cds, root_pr_nodes = get_earliest_principal_node(cds))
+  } else {
+    cds <- order_cells(cds)
+  }
+
+
+  # Save the pseudotime data and the CDS object to files
+  saveRDS(cds, cds_filename)
+
+
+  # Create and save plots
+  gene_plot <- plot_cells(cds = cds, genes = gene_expression, 
+                          cell_size = point_size, graph_label_size = 4.5,
+                          label_leaves = FALSE, label_branch_points = FALSE,
+                          label_cell_groups = FALSE,
+                          alpha = 1,
+                          label_roots = FALSE)
+  gene_plot <- gene_plot + theme(
+    plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+    plot.subtitle = element_blank(),
+    axis.line.x = element_line(size = 1.25, lineend = "round"),
+    axis.line.y = element_line(size = 1.25, lineend = "round"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    axis.ticks.length = unit(0.2, "cm"),
+    axis.ticks = element_line(size = 1),
+    legend.title = element_text(size = 14, vjust = 1),
+    strip.text = element_blank(),
+    legend.text = element_text(size = 12)
+  ) +
+    ggtitle(paste0(gene_title, " Expression"))+
+    coord_fixed(ratio = 1, xlim = c(-15, 15), ylim = c(-15, 15))
+
+  ggsave(
+    plot = gene_plot, device = plot_type, filename = plot_file,
+    width = plot_width, height = plot_height, units = plot_unit, dpi = plot_dpi
+  )
   
-  #Integrating the new pseudotime data into the count matrix.
-  #Here I extract just the pseudotime calculations from the cds
-  #object to a use for the swithde function calculation later.
-  #The output of this code is a data frame that has 2 columns (pseudotime
-  #value and sample name[cell name]) and 375 rows
-  #(cells with their associated pseudotime).
+
+  pt_graph <- plot_cells(cds,
+    color_cells_by = "pseudotime",
+    label_cell_groups = FALSE,
+    label_leaves = FALSE,
+    label_branch_points = FALSE,
+    graph_label_size = 4.5,
+    cell_size = point_size,
+    alpha = 1
+  )
+  pt_graph <- pt_graph +
+    theme(plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+          axis.line.x = element_line(size = 1.25, lineend = "round"),
+          axis.line.y = element_line(size = 1.25, lineend = "round"),
+          axis.text = element_text(size = 12),
+          axis.title = element_text(size = 14),
+          axis.ticks.length = unit(0.2, "cm"),
+          axis.ticks = element_line(size = 1),
+          legend.title = element_text(size = 14, vjust = 1),
+          legend.text = element_text(size = 12)) +
+    ggtitle(paste0(pt_title, " Pseudotime"))+
+    scale_color_viridis_c(option = "C") +
+    labs(color = "Pseudotime")+
+    coord_fixed(ratio = 1, xlim = c(-15, 15), ylim = c(-15, 15))
+
+  ggsave(
+    plot = pt_graph, device = plot_type, filename = pt_file,
+    width = plot_width, height = plot_height, units = plot_unit, dpi = plot_dpi
+  )
+  
+
+  # Saving pseudotime data to a dataframe
   pseudotime_data <- cds@principal_graph_aux@listData[["UMAP"]][["pseudotime"]]
   pseudotime_data <- as.data.frame(pseudotime_data)
   colnames(pseudotime_data) <- "Pseudotime"
   pseudotime_contents <- rownames(pseudotime_data)
   pseudotime_data$Samples <- pseudotime_contents
-  
-  #Saving the  various plots to .svg or other specified format
-  #with particular parameters
-  
-  #The pseudotime graph
-  ggsave(filename = my_monocle_graph,
-         plot     = print(pt_graph, newpage = FALSE),
-         device   = my_moncole_plot_type, dpi = my_monocle_plot_dpi,
-         width    = my_monocle_width, height = my_monocle_height,
-         units    = my_monocle_unit)
-  
-  #The gene expression graph
-  ggsave(filename = my_monocle_graph_genes,
-         plot     = print(vim_plot, newpage = FALSE),
-         device   = my_moncole_plot_type, dpi = my_monocle_plot_dpi,
-         width    = my_monocle_width, height = my_monocle_height,
-         units    = my_monocle_unit)
-  
-  #Saving the relevant R objects and data frames
-  saveRDS(cds, my_cds_filename)
-  write.csv(pseudotime_data, my_pt_data_filename)
-  
-  #Returning our objects
-  cds_return_list[["CDS"]] <- cds
-  cds_return_list[["Pseudotime"]] <- pseudotime_data
-  cds_return_list[["Cell Progression Graph"]] <- vim_plot
-  cds_return_list[["PT Graph"]] <- pt_graph
-  return(cds_return_list)
+
+  # Return a list of the relevant objects
+  return(list(CDS = cds, Cell_Progression_Graph = gene_plot, PT_Graph = pt_graph, PT_Data = pseudotime_data))
 }
--- Code/lung_mirna_calculator.R
+++ Code/lung_mirna_calculator.R
@@ -1,8 +1,3 @@
-# Name: mirna_calculator.R
-# Author: Andrew Willems <awillems@vols.utk.edu>
-# Purpose: Efficiently build miRNA metric outputs
-
-# mirna_calculator
 mirna_calculator <- function(ts_org = "Human",
                              ts_version = "8.0",
                              max_mir_targets = 10,
@@ -10,170 +5,91 @@ mirna_calculator <- function(ts_org = "Human",
                              cancer_type1 = "lung cancer",
                              print_ts_targets = TRUE,
                              mirna_remove = "hsa-miR-129-1-3p",
-                             max_mirnas = 807,
+                             max_mirnas = 808,
                              save_mirna_genes = TRUE,
                              save_mirna_genes_mat = TRUE,
                              include_sites_score = FALSE,
                              mirna_genes_mat_name = "Outputs/lung_mirna_score_matrix.rds",
                              mirna_ranking_name = "Outputs/lung_mirnas.rds") {
-
   library(hoardr)
+
+
+  # Set up the number of cores for parallelization
+  num_cores <- detectCores() - 1
+
+
   # miRNAs from miRmap
-  mirmap_mirnas <- read.csv(
-    file = "Data/miRNA-data/mirmap_mirnas.csv",
-    sep = ","
-  )
+  mirmap_mirnas <- read.csv("Data/mirna_data/mirmap_mirnas.csv", sep = ",")
 
-  if (cancer_up == TRUE) {
-    dbdemc_high <- read.csv(
-      file = "Data/miRNA-data/dbDEMC-2.0-high.txt",
-      sep = "\t"
-    )
+  # Read and filter cancer data
+  cancer_data <- read.csv(file = if (cancer_up) "Data/mirna_data/dbdemc_2.0_high.txt" else "Data/mirna_data/dbdemc_2.0_low.txt", sep = "\t")
+  cancer_data <- filter(cancer_data, Cancer.Type == cancer_type1)
 
-    # Filtering to just the miRNAs associated with a particular type of cancer
-    dbdemc_high <- filter(
-      dbdemc_high,
-      Cancer.Type == cancer_type1
-    )
-    dbdemc_high_mirnas <- subset(dbdemc_high, select = miRBase.Update.ID)
-    if ("unknown" %in% dbdemc_high_mirnas$miRBase.Update.ID == TRUE) {
-      dbdemc_high_mirnas <- filter(
-        dbdemc_high_mirnas,
-        miRBase.Update.ID != "unknown"
-      )
-      dbdemc_high_mirnas <- as.vector(dbdemc_high_mirnas)
-    } else {
-      dbdemc_high_mirnas <- as.vector(dbdemc_high_mirnas)
-    }
+  # Extract miRNA names
+  cancer_mirnas <- unique(cancer_data$miRBase.Update.ID)
+  cancer_mirnas <- cancer_mirnas[cancer_mirnas != "unknown"]
 
-    # Common miRNAs
-    common_mirnas <- intersect(
-      mirmap_mirnas$mature_name,
-      dbdemc_high_mirnas$miRBase.Update.ID
-    )
-  } else {
-    dbdemc_low <- read.csv(
-      file = "Data/miRNA-data/dbDEMC-2.0-low.txt",
-      sep = "\t"
-    )
-    colnames(dbdemc_low)[1] <- "miRNA.ID"
-
-    # Filtering to just the miRNAs associated with a particular type of cancer
-    dbdemc_low <- filter(
-      dbdemc_low,
-      Cancer_Type == cancer_type1
-    )
-    dbdemc_low_mirnas <- subset(dbdemc_low, select = miRBase_update)
-    dbdemc_low_mirnas <- as.vector(dbdemc_low_mirnas)
-
-    # Common miRNAs between databases
-    common_mirnas <- intersect(
-      mirmap_mirnas$mature_name,
-      dbdemc_low_mirnas$miRBase_update
-    )
-  }
-
-  # Now submitting these miRNAs to TargetScan to get genes to make a gene list
-  # for the third metric testing to see if all of the miRNAs exist in TargetScan
-  # before getting all submitted at once
+  # Common miRNAs
+  common_mirnas <- intersect(mirmap_mirnas$mature_name, cancer_mirnas)
   common_mirnas <- common_mirnas[!common_mirnas %in% mirna_remove]
   saveRDS(common_mirnas, file = "Outputs/common_mirnas.rds")
-  if (length(common_mirnas) >= length(common_mirnas[1:max_mirnas])) {
+
+  # Limit number of miRNAs
+  if (length(common_mirnas) > max_mirnas) {
     common_mirnas <- common_mirnas[1:max_mirnas]
-  } else {
-    print("There are fewer target mirnas available than your input. Using the largest number of common mirnas for this submission to TargetScan")
-    common_mirnas <- common_mirnas[1:length(common_mirnas)]
-    print(paste0("The number of common mirnas is: ", length(common_mirnas)))
+  } else if (print_ts_targets) {
+    cat("There are fewer target mirnas available than your input. Using the largest number of common mirnas for this submission to TargetScan\n")
+    cat("The number of common mirnas is:", length(common_mirnas), "\n")
   }
 
-  if (print_ts_targets == TRUE) {
-    print(length(common_mirnas))
-  }
-  my_num <- 1
-  mirna_targets <- vector(mode = "list", length = length(common_mirnas))
-  for (m in common_mirnas[1:length(common_mirnas)]) {
-    percent_done <- (my_num / length(common_mirnas)) * 100
-    print(paste0("Currently ", round(percent_done, digits = 2), "% done"))
-    print(paste0("We are currently on miRNA: ", m))
-    current_target <- hoardeR::targetScan(
-      mirna = common_mirnas[my_num],
+  # Get TargetScan results
+  mirna_targets <- mclapply(common_mirnas, function(m) {
+    hoardeR::targetScan(
+      mirna = m,
       species = ts_org,
       release = ts_version,
       maxOut = max_mir_targets
-    )
+    ) %>%
+      mutate(miRNA_name_final = m)
+  })
 
-    mirna_name <- m
-    mirna_name_final <- rep(mirna_name, times = length(current_target$Ortholog))
-    current_target <- cbind(current_target, mirna_name_final)
-    mirna_targets[[m]] <- current_target
-    my_num <- my_num + 1
-  }
+  # Simplify TargetScan results
+  total_list <- lapply(mirna_targets, function(df) {
+    df %>%
+      select(Ortholog, miRNA_name_final) %>%
+      rename(gene_list = Ortholog, mirna_list = miRNA_name_final)
+  })
 
-
-
-  # Simplifying the output of the TargetScan commands to
-  # just the Gene name and the miRNA columns
-  counter <- 1
-  total_list <- list()
-  for (i in mirna_targets) {
-    current_df <- i
-    gene_list <- current_df$Ortholog
-    mirna_list <- current_df$miRNA_name_final
-    simple_list <- cbind(gene_list, mirna_list)
-    total_list[[counter]] <- simple_list
-    counter <- counter + 1
-  }
-
-  counter <- 1
-  all_mirs_for_score <- list()
-  all_genes_for_score <- list()
-  for (l in total_list) {
-    current_df <- l
-    current_mirs <- current_df[, "mirna_list"]
-    all_mirs_for_score[[counter]] <- unique(current_mirs)
-    current_genes <- current_df[, "gene_list"]
-    all_genes_for_score[[counter]] <- current_genes
-    counter <- counter + 1
-  }
-
-  all_mirs_for_score <- unlist(all_mirs_for_score)
-  all_genes_for_score <- unlist(all_genes_for_score)
-  all_genes_for_score_unique <- unique(all_genes_for_score)
-
-  mirna_score <- matrix(
-    data = 0, nrow = length(all_genes_for_score_unique),
-    ncol = length(all_mirs_for_score),
-    dimnames = list(
-      all_genes_for_score_unique,
-      all_mirs_for_score
-    )
+  # Create score matrix
+  all_mirs_for_score <- unique(unlist(sapply(total_list, "[[", "mirna_list")))
+  all_genes_for_score <- unique(unlist(sapply(total_list, "[[", "gene_list")))
+  mirna_score <- matrix(0,
+    nrow = length(all_genes_for_score), ncol = length(all_mirs_for_score),
+    dimnames = list(all_genes_for_score, all_mirs_for_score)
   )
-  mirna_score <- as.data.frame(mirna_score)
 
-  # Checking to see for each miRNA (column name) if it interacts with a particular row.
-  # If it does it get a plus one to that cell. If it does not it moves to next cell
-  for (i in rownames(mirna_score)) {
-    for (x in mirna_targets) {
-      current_df <- x
-      if (i %in% current_df$Ortholog) {
-        mirna_to_add <- unique(current_df$miRNA_name_final)
-        mirna_score[i, mirna_to_add] <- mirna_score[i, mirna_to_add] + 1
-      }
+  # Calculate scores
+  for (i in seq_along(total_list)) {
+    df <- total_list[[i]]
+    for (j in seq_len(nrow(df))) {
+      gene <- df[j, "gene_list"]
+      mirna <- df[j, "mirna_list"]
+      mirna_score[gene, mirna] <- mirna_score[gene, mirna] + 1
     }
   }
 
-  if (save_mirna_genes_mat == TRUE) {
+  # Save score matrix
+  if (save_mirna_genes_mat) {
     saveRDS(mirna_score, file = mirna_genes_mat_name)
   }
 
-  # Now calculating the row sums of each gene for total number of
-  # the miRNA interactions
-  mirna_gene_list <- rowSums(mirna_score)
-  mirna_ranking <- abs(mirna_gene_list) / sum(abs(mirna_gene_list))
+  # Calculate and save miRNA ranking
+  mirna_ranking <- rowSums(mirna_score) / sum(mirna_score)
   mirna_ranking <- sort(mirna_ranking, decreasing = TRUE)
-  if (save_mirna_genes == TRUE) {
+
+  if (save_mirna_genes) {
     saveRDS(mirna_ranking, file = mirna_ranking_name)
   }
-  # Return object
+
   return(mirna_ranking)
 }
